83fa2836276c96a9ba40623dacb514bbf9a26110
skellinghoff <Jarodwellinghoff@gmail.com>
2022-10-03 21:03:32 -0500

    asdf

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const userModel = require("./models");
+
+const app = express();
+const port = 80;
+
+const corsOptions = {
+    origin: 'https://my-tech-map.netlify.app',
+    credentials: true,
+};
+app.use(cors(corsOptions));
+
+mongoose.connect('mongodb+srv://mytechmap:SCPFall2022@cluster0.01a729l.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
+});
+
+// // Configuring body parser middleware
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+app.get('/details', async (req, res) => {
+    // const user = await userModel.findOne({id: 123});
+    const user = await userModel.find({});
+    res.send(user);
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
